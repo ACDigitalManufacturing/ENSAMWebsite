45e0305e53bc3e431bc1ce325f91554a01e4d225
Aala Eddine Bousskoul <70477189+aalaebl@users.noreply.github.com>
2021-11-12 07:30:07 +0100

    Adding 'Nos Formations' icons

---
+++ src/client/assets/icons/api.tsx
@@ -0,0 +1,16 @@
+export default (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="55.974"
+    height="41.981"
+    viewBox="0 0 55.974 41.981"
+  >
+    <path
+      id="iconmonstr-school-21"
+      d="M55.974,44.981h-7l2.332-7h2.332Zm-30.263-10.6,20.934-10.67V37.984c-2.349,4.76-13.077,7-19.824,7-7.4,0-17.233-2.318-19.824-7V22.558Zm27.931,1.266H51.31V18.719L25.834,31.705,0,15.38,27.987,3,53.642,17.532Z"
+      transform="translate(0 -3)"
+      fill="#286fac"
+      fill-rule="evenodd"
+    />
+  </svg>
+);
