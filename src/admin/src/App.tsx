ca2e5904ad2bea7d4e0ab16b22e86af874aff57f
Aala Eddine Bousskoul <70477189+aalaebl@users.noreply.github.com>
2021-12-05 14:07:19 +0100

    add react toastify

--- src/admin/src/App.tsx
+++ src/admin/src/App.tsx
@@ -1,7 +1,14 @@
 import RootRoutes from "routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = function (): JSX.Element {
-  return <RootRoutes />;
+  return (
+    <>
+      <RootRoutes />
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
