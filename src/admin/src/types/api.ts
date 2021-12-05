43c5711e5f4fad7e444dfeece3f88dcc7b4d6f6f
Aala Eddine Bousskoul <70477189+aalaebl@users.noreply.github.com>
2021-12-05 13:35:37 +0100

    images suppoeted types

--- src/admin/src/types/api.ts
+++ src/admin/src/types/api.ts
@@ -1,10 +1,19 @@
 // eslint-disable-next-line
 export interface Response<TResponse = any> {
-  response?: TResponse
-  errors?: CustomError[]
+  response?: TResponse;
+  errors?: CustomError[];
 }
 
 export interface CustomError {
-  message: string
-  field?: string
+  message: string;
+  field?: string;
+}
+
+export enum AllowedTypesForCover {
+  png = "image/png",
+  jpg = "image/jpg",
+  gif = "image/gif",
+  jpeg = "image/jpeg",
+  svg = "image/svg+xml",
+  webp = "image/webp",
 }
