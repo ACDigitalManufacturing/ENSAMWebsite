85fff3275ba9866b87dfb29a901b24d7c963349e
Aala Eddine Bousskoul <70477189+aalaebl@users.noreply.github.com>
2021-12-14 10:36:18 +0100

    Sidebar expand/minimize feature

---
+++ src/admin/src/assets/icons/navArrowLeft.tsx
@@ -0,0 +1,21 @@
+export default (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+  >
+    <g
+      id="Arrow_-_Right_Circle"
+      data-name="Arrow - Right Circle"
+      transform="translate(0 0)"
+    >
+      <path
+        id="Arrow_-_Right_Circle-2"
+        data-name="Arrow - Right Circle"
+        d="M10,20l-.28,0A10.011,10.011,0,1,1,10,20ZM8.56,5.78a.745.745,0,0,0-.53,1.28L10.98,10,8.03,12.94A.746.746,0,0,0,8.02,14a.752.752,0,0,0,.534.225A.727.727,0,0,0,9.08,14l3.49-3.47a.75.75,0,0,0,0-1.06L9.08,6A.7.7,0,0,0,8.56,5.78Z"
+        transform="translate(0 0)"
+      />
+    </g>
+  </svg>
+);
