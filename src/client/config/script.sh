78aa59d27b3adf691b64b637223520030da4a5f5
Karim G <karim2jihad@gmail.com>
2021-12-15 22:10:01 +0100

    configure nginx and CI (#35)

--- src/client/config/script.sh
+++ src/client/config/script.sh
@@ -1,13 +1,7 @@
 # configure nginx
 
-sudo su
-
-cp \
+sudo cp \
     ./ensam-website-nginx.conf \
     /etc/nginx/sites-available/ensam-website-nginx.conf
 
-ln -s \
-    /etc/nginx/sites-available/ensam-website-nginx.conf \
-    /etc/nginx/sites-enabled/ensam-website-nginx.conf
-
-systemctl reload nginx
+sudo systemctl reload nginx
