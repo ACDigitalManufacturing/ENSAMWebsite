efd286b6e09ad8a298b7cc336c5bf3ef6be503ad
Karim G <karim2jihad@gmail.com>
2021-12-15 18:51:29 +0100

    fix shell script

--- src/client/config/run.sh
+++ src/client/config/run.sh
@@ -1,8 +1,11 @@
 cd ../
+sh ./script.sh
 sudo npm install pm2@latest -g
 pm2 delete all
+cd ../server
+npm install
+pm2 start "npm start"
 cd ./client
+npm install
 npm run build
 pm2 start "npm start"
-cd ../server
-npm start
