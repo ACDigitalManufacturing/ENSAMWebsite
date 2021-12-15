22f22c1c64bc391e23524ef2fa4dcbda5fe8ac2c
Karim G <karim2jihad@gmail.com>
2021-12-15 19:09:33 +0100

    update shell script

--- src/client/config/run.sh
+++ src/client/config/run.sh
@@ -1,11 +1,31 @@
 cd ../
-sh script.sh
+echo "# nginx configuration start"
+./script.sh
+echo "# nginx configuration done"
+
+echo "# installing pm2"
 sudo npm install pm2@latest -g
+echo "# finish installation of pm2"
+echo "# clear all pm2 servers already runing"
 pm2 delete all
+
+echo "############################"
+echo "#                          #"
+echo "# install and start server #"
+echo "#                          #"
+echo "############################"
+
 cd ./server
 npm install -f
-pm2 start "npm start"
+pm2 start npm --name "server" --interpreter bash -- start
+
+echo "############################"
+echo "#                          #"
+echo "# install and start client #"
+echo "#                          #"
+echo "############################"
+
 cd ../client
 npm install -f
 npm run build
-pm2 start "npm start"
+pm2 start npm --name "client" --interpreter bash -- start
