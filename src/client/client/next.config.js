ae5e92feb88ee8121ff425c3e7c06b1950ad681f
Karim G <karim2jihad@gmail.com>
2022-07-14 22:44:45 +0100

    disable swcMinify

--- src/client/client/next.config.js
+++ src/client/client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: false,
   env: {
     NEXT_PUBLIC_BACKEND_API_URI:
       "https://ensam-casa-server.herokuapp.com/api/v1",
