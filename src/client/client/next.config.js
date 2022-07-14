24988b4e5312841aed0c35f109c8229c956855da
Karim G <karim2jihad@gmail.com>
2022-07-14 21:46:35 +0100

    update next.config.js

--- src/client/client/next.config.js
+++ src/client/client/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env:{
-    NEXT_PUBLIC_BACKEND_API_URI:"http://ensam-casa.ma/api/v1"
-  }
-}
+  env: {
+    NEXT_PUBLIC_BACKEND_API_URI:
+      "https://ensam-casa-server.herokuapp.com/api/v1",
+  },
+};
